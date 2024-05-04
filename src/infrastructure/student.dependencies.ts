cb039bca7b8c417de3e6c239b836dbc677bcb631
Mauricio Cruz <mawcp7@gmail.com>
2024-05-04 12:24:17 -0600

    chore: Add GetStudentByIdUseCase and GetTutorByIdUseCase to student dependencies

--- src/infrastructure/student.dependencies.ts
+++ src/infrastructure/student.dependencies.ts
@@ -9,9 +9,12 @@ import { AddStudentToTutorUseCase } from "../application/use-cases/add-student-t
 import { AddSubjectToStudentUseCase } from "../application/use-cases/add-subject-to-student.use-case";
 import { GetStudentsFromTutorUseCase } from "../application/use-cases/get-students-from-tutor.use-case";
 import { GetSubjectsFromStudentUseCase } from "../application/use-cases/get-subjects-from-student.use-case";
+import { GetStudentByIdUseCase } from "../application/use-cases/get-student-by-id.use-case";
+import { GetTutorByIdUseCase } from "../application/use-cases/get-tutor-by-id.use-case";
 
 const mySqlStudentRepository = new MysqlStudentRepository();
 
+const getStudentByIdUseCase = new GetStudentByIdUseCase(mySqlStudentRepository);
 const createSubjectUseCase = new CreateSubjectUseCase(mySqlStudentRepository);
 const createTutorUseCase = new CreateTutorUseCase(mySqlStudentRepository);
 const createStudentUseCase = new CreateStudentUseCase(mySqlStudentRepository);
@@ -21,5 +24,6 @@ const addStudentToTutorUseCase = new AddStudentToTutorUseCase(mySqlStudentReposi
 const addSubjectToStudentUseCase = new AddSubjectToStudentUseCase(mySqlStudentRepository);
 const getStudentsFromTutorUseCase = new GetStudentsFromTutorUseCase(mySqlStudentRepository);
 const getSubjectsFromStudentUseCase = new GetSubjectsFromStudentUseCase(mySqlStudentRepository);
+const getTutorByIdUseCase = new GetTutorByIdUseCase(mySqlStudentRepository);
 
-export const studentController = new StudentController(getStudentsFromTutorUseCase, getSubjectsFromStudentUseCase, addStudentToTutorUseCase, addSubjectToStudentUseCase, createSubjectUseCase, createStudentUseCase, createTutorUseCase, getTutorsUseCase, getStudentsUseCase);
+export const studentController = new StudentController(getTutorByIdUseCase, getStudentByIdUseCase, getStudentsFromTutorUseCase, getSubjectsFromStudentUseCase, addStudentToTutorUseCase, addSubjectToStudentUseCase, createSubjectUseCase, createStudentUseCase, createTutorUseCase, getTutorsUseCase, getStudentsUseCase);
